0908224bd52034c5645e9e4c29715539d63114d3
Mauro Panzini <mauropanzini18@gmail.com>
2024-05-15 19:01:13 -0300

    Agregadas consignas en archivos

--- Clase03/Ejercicio20bis/registro.php
+++ Clase03/Ejercicio20bis/registro.php
@@ -1,5 +1,16 @@
 <?php
-
+/*
+Mauro Panzini - A332-1 LUNES
+Aplicación No 20 BIS (Registro CSV)
+Archivo: registro.php
+método:POST
+Recibe los datos del usuario(nombre, clave,mail )por POST ,
+crear un objeto y utilizar sus métodos para poder hacer el alta,
+guardando los datos en usuarios.csv.
+retorna si se pudo agregar o no.
+Cada usuario se agrega en un renglón diferente al anterior.
+Hacer los métodos necesarios en la clase usuario
+*/
 include 'Usuario.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
